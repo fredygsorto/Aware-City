1256652d1278470961ace03525acddc14b720df5
fredygsorto <fsorto@nyit.edu>
2023-02-21 19:09:32 -0500

    Made routes folder and added BottomTab.js file

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from "./screens/Home"; 
-import MapScreen from "./screens/MapScreen"; 
-import Charity from "./screens/Charity"; 
-import Ionicons from 'react-native-vector-icons/Ionicons'; 
-
-const Tab = createBottomTabNavigator(); 
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./screens/Home";
+import MapScreen from "./screens/MapScreen";
+import Charity from "./screens/Charity";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import BottomTab from "./routes/BottomTab";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-    <Tab.Navigator 
-        initialRouteName='Home'
-        screenOptions={({ route }) => ({
-          headerShown: true,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'MapScreen') {
-              iconName = focused ? 'map' : 'map-outline';
-            } else if (route.name === 'Charity'){
-              iconName = focused ? 'heart-half' : 'heart-half-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#CD7F32',
-          tabBarInactiveTintColor: '#000000',
-        })}>
-      <Tab.Screen name ="Home" component={Home}/>
-      <Tab.Screen name ="MapScreen" component={MapScreen} options={{tabBarBadge: 1}}/>
-      <Tab.Screen name ="Charity" component={Charity}/>
-    </Tab.Navigator>
-    </NavigationContainer>
-  ); 
-  
+  return <BottomTab />;
 }
