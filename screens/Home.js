34bf687ac2a4a7e70ec3d595ccefe66a3e1ce735
fredygsorto <fsorto@nyit.edu>
2023-02-22 21:40:46 -0500

    Rename assets

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { markers } from "../MapData";
 
 export default function Home() {
-  const logo = require("../assets/ac_no_bg.png");
+  // const logo = require("../assets/ac_no_bg.png");
 
   return (
     <View style={styles.container}>
-      <Image style={styles.logo} source={logo} />
+      {/* <Image style={styles.logo} source={logo} /> */}
       <Text>This is the home page</Text>
+      {/* <FlatList
+        data={markers}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        )}
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,13 +27,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    paddingVertical: 25,
-    justifyContent: "flex-start",
-    flexDirection: "column",
-    backgroundColor: "#fff",
+    justifyContent: "center",
   },
   logo: {
-    width: "90%",
-    height: "10%",
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  listItem: {
+    backgroundColor: "#FCE9D8",
+    padding: 30,
+    marginVertical: 8,
+    marginHorizontal: 1,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 16,
   },
 });
