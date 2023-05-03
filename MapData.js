9e7e04d3e3d74569374ea554640141ca438f58da
vaderlock <smuntase@nyit.edu>
2023-05-03 01:01:36 -0400

    brooklyn homeless shelters added

--- MapData.js
+++ MapData.js
@@ -97,4 +97,184 @@ export const markers = [
     address: "225 E 53rd St, New York, NY 10022",
     type:"homeless_shelter",
   },
+  // Brooklyn homeless shelters: 
+{
+  coordinate: {
+    latitude: 40.689999,
+    longitude: -73.932732,
+  },
+  pinColor: "blue",
+  title: "Providence House",
+  description: "Transitional Housing and Shelter at Providence House",
+  address: "703 Lexington Avenue, Brooklyn, NY 11221",
+  type:"homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.662899,
+    longitude: -73.982918,
+  },
+  pinColor: "blue",
+  title: "Park Slope Armory YMCA",
+  description: "Women's Shelter Brooklyn",
+  address: "1402 8th Ave, Brooklyn, NY 11215",
+  type:"homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.6945228,
+    longitude: -73.9767989,
+  },
+  pinColor: "blue",
+  title: "Auburn Housing",
+  description: "Family Housing and Homeless Shelter",
+  address: "39 Auburn Place, Brooklyn, NY 11205",
+  type:"homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.698111,
+    longitude: -73.983532,
+  },
+  pinColor: "blue",
+  title: "CAMBA men's Shelter",
+  description: "Brooklyn - Single Adult Men",
+  address: "59-65 Prince Street, Brooklyn, NY 11201",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.669669,
+    longitude: -73.887812,
+  },
+  pinColor: "blue",
+  title: "Brooklyn HELP",
+  description: "Women's 24 hour Shelter",
+  address: "104-152 Williams Ave, Brooklyn, NY",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.694888,
+    longitude: -73.926286,
+  },
+  pinColor: "blue",
+  title: "Broadway House",
+  description: "Women's Shelter CAMBA - Temporary Shelter",
+  address: "1245 Broadway Avenue, Brooklyn, NY 11221",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.675599,
+    longitude: -73.905040,
+  },
+  pinColor: "blue",
+  title: "CAMBA Gathering Place",
+  description: "Drop In Center for Homeless Adults",
+  address: "2402 Atlantic Ave, Brooklyn, NY 11233",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.715580,
+    longitude: -73.941609,
+  },
+  pinColor: "blue",
+  title: "Barbara Kleiman Residence",
+  description: "Homeless Shelter",
+  address: "300 Skillman Ave, Brooklyn, NY 11211",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.675717,
+    longitude: -73.904777,
+  },
+  pinColor: "blue",
+  title: "Atlantic House",
+  description: "Men's Shelter - CAMBA",
+  address: "2402 Atlantic Avenue, Brooklyn, NY 11233",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.682732,
+    longitude: -73.897204,
+  },
+  pinColor: "blue",
+  title: "Van Siclen Family Residence",
+  description: "Transitional Housing for Homeless Families with Children",
+  address: "246 Jamaica Avenue, Brooklyn, NY 11207",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.696765,
+    longitude: -73.986035,
+  },
+  pinColor: "blue",
+  title: "Tillary Street",
+  description: "Women's Shelter",
+  address: "200 Tillary Street, Brooklyn, NY 11201",
+  type: "homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.678799,
+    longitude: -73.912082,
+  },
+  pinColor: "blue",
+  title: "Magnolia House",
+  description: "Women's Shelter",
+  address: "1424 Herkimer St, Brooklyn, NY 11233",
+  type:"homeless_shelter",
+},
+{
+  coordinate: {
+    latitude: 40.679770,
+    longitude: -73.938017,
+  },
+  pinColor: "blue",
+  title: "Neighbors Together Corporation",
+  description: "Homeless Shelter",
+  address: "2094 Fulton Street, Brooklyn, NY 11233",
+  type:"homeless_shelter",
+},
+
+{
+  coordinate: {
+    latitude: 40.677304,
+    longitude: -73.912507,
+  },
+  pinColor: "blue",
+  title: "The Gathering Place",
+  description: "Homeless Shelter",
+  address: "2402 Atlantic Avenue, Brooklyn, NY",
+  type:"homeless_shelter",
+},
+
+{
+  coordinate: {
+    latitude: 40.698937,
+    longitude: -73.902397,
+  },
+  pinColor: "blue",
+  title: "H.E.L.P.",
+  description: "Women's Center",
+  address: "116 Williams Avenue, Brooklyn, NY",
+  type:"homeless_shelter",
+},
+
+{
+  coordinate: {
+    latitude: 40.683701,
+    longitude: -73.956684,
+  },
+  pinColor: "blue",
+  title: "Lexington Avenue Residence - Bed-Stuy",
+  description: "Homless Shelter",
+  address: "85 Lexington Avenue, Brooklyn, NY - 11238",
+  type:"homeless_shelter",
+}
 ];
